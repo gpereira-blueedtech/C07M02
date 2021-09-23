832abd302ebec8527478bd51a01453319db952c5
Gabriel Lima <gpereira@blueedtech.com.br>
2021-09-22 22:53:58 -0300

    Exemplos usados na aula 03

---
+++ Aula03/script.js
@@ -0,0 +1,9 @@
+const btnTexto = document.querySelector("#btn-texto"); 
+
+window.onload = function() {
+    alert("Bem vindo ao meu site!!");
+};
+
+btnTexto.addEventListener("click", function () {
+    alert("Funcionou!!!");
+})
